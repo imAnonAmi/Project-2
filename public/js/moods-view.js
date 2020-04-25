9607b74958ecbbd0adfdf5807cb9ead78fb5200c
trapezechic <ingridhoffman@me.com>
2020-04-25 14:33:03 -0700

    working toward cloud view

--- public/js/moods-view.js
+++ public/js/moods-view.js
@@ -7,19 +7,57 @@ $(document).ready(function() {
 	userId = url.split("=")[1];
 	getEntries(userId);
 
-	// This function grabs posts from the database and updates the view
+	// Function to get entries from database and display on page
 	function getEntries(user) {
 		userId = user || "";
 		if (userId) {
 			userId = "/?user_id=" + userId;
 		}
-		$.get("/api/posts" + userId, (data) => {
-			let entries = data;
-			if (!entries || !entries.length) {
+		$.get("/api/moods" + userId, (data) => {
+			if (!data || !data.length) {
 				alert("No entries!");
 			} else {
-				alert("Here are your entries.");
+				// create string from all data.moods entries
+				let moodString = "";
+				// send to moodCloud function
+				moodCloud(moodString);
 			}
 		});
+	}
+
+	// Function to display moods as word cloud
+	function moodCloud(moodString) {
+		fetch("https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud", {
+			method: "POST",
+			headers: {
+				"x-rapidapi-host": "textvis-word-cloud-v1.p.rapidapi.com",
+				"x-rapidapi-key": "442fbf0644mshed26ef444ee0de1p1dbed6jsn2dec5654b644",
+				"content-type": "application/json",
+				accept: "application/json",
+			},
+			body: JSON.stringify({
+				text: moodString,
+				scale: 1,
+				width: 800,
+				height: 800,
+				colors: ["#375E97", "#FB6542", "#FFBB00", "#3F681C"],
+				font: "Tahoma",
+				use_stopwords: true,
+				language: "en",
+				uppercase: false,
+			}),
+		})
+			.then((response) => {
+				return response.text();
+			})
+			.then((wordCloud) => {
+				var img = document.getElementById("wordCloud");
+				img.src = wordCloud;
+				img.height = 800;
+				img.width = 800;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
 });
