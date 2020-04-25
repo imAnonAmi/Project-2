5fd63878d6f32722753089d5b467d30c4c984c25
imAnonAmi <mcintireaw@gmail.com>
2020-04-25 12:51:58 -0700

    Working moodEntry.js

--- models/moodEntry.js
+++ models/moodEntry.js
@@ -1,18 +1,35 @@
-// module.exports = function(sequelize, DataTypes) {
-// 	const MoodEntry = sequelize.define("MoodEntry", {
-// 		// create database model
+module.exports = function(sequelize, DataTypes) {
+	const MoodEntry = sequelize.define("MoodEntry", {
+		// create database model
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+          },
+          moods: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+          },
+          journal: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+          },
 
-// 	});
+	});
 
-// 	// Post.associate = function(models) {
-// 	// 	// We're saying that a Post should belong to an Author
-// 	// 	// A Post can't be created without an Author due to the foreign key constraint
-// 	// 	Post.belongsTo(models.Author, {
-// 	// 		foreignKey: {
-// 	// 			allowNull: false,
-// 	// 		},
-// 	// 	});
-// 	// };
+	MoodEntry.associate = function(models) {
+	// 	// We're saying that a Post should belong to an Author
+	// 	// A Post can't be created without an Author due to the foreign key constraint
+		MoodEntry.belongsTo(models.User, {
+			foreignKey: {
+				allowNull: false,
+			},
+		});
+	};
 
-// 	return MoodEntry;
-// };
+	return MoodEntry;
+};
